f8c8ceff28355912584ef61cfb413aa211c86d1f
Joely <sjoely131@gmail.com>
2023-05-04 08:11:30 -0300

    Versão 1 do projeto de gestão de festa

---
+++ src/test/java/com/algoworks/festa/GestaoFesta1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algoworks.festa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFesta1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
